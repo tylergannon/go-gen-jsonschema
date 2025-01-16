3e9e9e8d3e52d287189dfb8528392c33965185fd
Tyler Gannon <tyler@pagerguild.com>
2025-01-16 14:39:10 -0600

    Add test case

---
+++ internal/builder/testfixtures/enums/enumsremote/enums.go
@@ -0,0 +1,16 @@
+package enumsremote
+
+// EnumType is an enum type from enumsremote
+type EnumType string
+
+const (
+	// EnumVal1 is a value!!
+	EnumVal1 EnumType = "val1"
+	// EnumVal2 is also a value!!
+	EnumVal2 EnumType = "val2"
+	// EnumVal3 is truly a value!!
+	EnumVal3 EnumType = "val3"
+)
+
+// EnumVal4 is the fourth value
+const EnumVal4 EnumType = "val4"
