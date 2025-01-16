8167985c40ad26cf31e5e100d9f48abb678b08b0
Tyler Gannon <tyler@pagerguild.com>
2025-01-16 16:19:31 -0600

    Add test case

---
+++ internal/builder/testfixtures/structs/struct_types.go
@@ -0,0 +1,45 @@
+package structs
+
+import (
+	_ "github.com/dave/dst"
+)
+
+// It's really that way
+type EnumType123 string
+
+const (
+	Var1 EnumType123 = "var1"
+	Var2 EnumType123 = "var2"
+	Var3 EnumType123 = "var3"
+	Var4 EnumType123 = "var4"
+)
+
+// Here's what the StructType1 is all about
+type StructType1 struct {
+	// Foobar brain baz bag
+	Field1 string `json:"field1"`
+	// Test field 2
+	Field2 string `json:"field2"`
+	// More comments
+	Field3 string `json:"field3"`
+	// Tell me about zoos and things
+	Field4 string `json:"field4"`
+
+	Field5 [][]struct {
+		// The second field is truly interesting.
+		Field2 []EnumType123 `json:"field2"`
+		Field3 struct {
+			Field9 []*EnumType123 `json:"field9"`
+			// foobar is just a field where you do things
+			Foobar string `json:"foobar"`
+		}
+	} `json:"field5"`
+}
+
+// Tell me a story about fairy tails and other things
+type StructType2 struct {
+	StructType1
+
+	// NestedStruct is very interesting
+	NestedStruct []StructType1 `json:"nestedStruct"`
+}
