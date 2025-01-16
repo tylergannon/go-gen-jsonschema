d523bbdacc2889f37ab4858e827c10fab7123f5f
Tyler Gannon <tyler@pagerguild.com>
2025-01-16 10:50:24 -0600

    Add subcommand for generating input files

--- gen-jsonschema/main.go
+++ gen-jsonschema/main.go
@@ -1,16 +1,70 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
+	"github.com/tylergannon/go-gen-jsonschema/internal/builder"
+	"github.com/tylergannon/go-gen-jsonschema/internal/scanner"
+	"go/build"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-func main() {
-	// Define the --pretty flag
-	pretty := flag.Bool("pretty", false, "Enable pretty output")
+//go:embed tmpl/config.go.tmpl
+var configTmplContents string
 
-	// Parse the CLI arguments
-	flag.Parse()
+func main() {
+	// Check for --help without a subcommand
+	if len(os.Args) < 2 || (os.Args[1][0] == '-' && os.Args[1] == "--help") {
+		printGlobalHelp()
+		return
+	}
+
+	// Extract the subcommand
+	subcommand := os.Args[1]
+
+	// Switch on the subcommand
+	switch subcommand {
+	case "gen":
+		handleGen()
+	case "new":
+		handleNew()
+	default:
+		fmt.Printf("Unknown subcommand: %s\n\n", subcommand)
+		printGlobalHelp()
+		os.Exit(1)
+	}
+}
+
+// Prints global help for the script
+func printGlobalHelp() {
+	fmt.Println("Usage:")
+	fmt.Println("  [subcommand] [options]")
+	fmt.Println("\nSubcommands:")
+	fmt.Println("  gen      Generate output (default)")
+	fmt.Println("  new      Create a new project")
+	fmt.Println("\nRun '[subcommand] --help' for more details.")
+}
+
+func handleGen() {
+	// Define the --pretty flag
+	genCmd := flag.NewFlagSet("gen", flag.ExitOnError)
+	pretty := genCmd.Bool("pretty", false, "Enable pretty output")
+
+	// Check if --help was requested
+	if len(os.Args) > 2 && os.Args[2] == "--help" {
+		fmt.Println("Usage: gen [options]")
+		fmt.Println("\nOptions:")
+		genCmd.PrintDefaults()
+		return
+	}
+
+	// Parse flags for the "gen" subcommand
+	genCmd.Parse(os.Args[2:])
 
 	// Use the flag value
 	if *pretty {
@@ -18,4 +72,133 @@ func main() {
 	} else {
 		fmt.Println("Pretty output is disabled.")
 	}
+}
+
+func handleNew() {
+	// Define the --out flag
+	var (
+		newCmd  = flag.NewFlagSet("new", flag.ExitOnError)
+		out     = newCmd.String("out", "", "Path to output file.  Empty val or '--' means print to stdout")
+		pkg     = newCmd.String("pkg", "", "Package for generated file. Default is current directory or using the package name for the package specified in --out")
+		methods = newCmd.String("methods", "", "Comma-separated list of methods to generate in the form of TypeName=MethodName,TypeName2=MethodName2")
+	)
+
+	// Check if --help was requested
+	if len(os.Args) > 2 && os.Args[2] == "--help" {
+		fmt.Println("Usage: new [options] FILENAME")
+		fmt.Println("\nOptions:")
+		newCmd.PrintDefaults()
+		return
+	}
+
+	// Parse flags for the "new" subcommand
+	var (
+		err       = newCmd.Parse(os.Args[2:])
+		args      = newCmd.Args()
+		pkgName   string
+		useStdout = *out == "" || *out == "--"
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Remaining args (after parsing)
+
+	if useStdout {
+		if *pkg != "" {
+			pkgName = *pkg
+		} else {
+			var wd string
+			if wd, err = os.Getwd(); err != nil {
+				log.Fatalln(err)
+			}
+			if pkgName, err = getPackageName(wd); err != nil {
+				log.Fatalln(err)
+			}
+		}
+	} else if pkgName, err = getPackageName(filepath.Dir(*out)); err != nil {
+		log.Fatalln(err)
+	}
+
+	var tmplArg = configArg{
+		BuildTag: scanner.BuildTag,
+		PkgName:  pkgName,
+	}
+
+	for _, methodArg := range strings.Split(*methods, ",") {
+		if len(methodArg) == 0 {
+			continue
+		}
+		parts := strings.SplitN(methodArg, "=", 2)
+		if len(parts) != 2 {
+			log.Fatalln("Invalid method argument.  Must be keyvalue in the form TypeName=MethodName. -- ", methodArg)
+		}
+		tmplArg.Methods = append(tmplArg.Methods, methodDef{
+			TypeName:   parts[0],
+			MethodName: parts[1],
+		})
+	}
+	if len(tmplArg.Methods) == 0 {
+		log.Fatalln("No methods to generate.")
+	}
+
+	fmt.Println("Package name:", pkgName)
+
+	writer, err := getOutputWriter(*out)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer writer.Close()
+	data, err := builder.RenderTemplate(configTmplContents, tmplArg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if formatted, err := builder.FormatCodeWithGoimports(data.Bytes()); err != nil {
+		log.Fatalln(err)
+	} else if _, err = writer.Write(formatted); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Perform the "new" command action
+	if len(args) > 0 {
+		fmt.Printf("Creating new project: %s\n", args[0])
+	} else {
+		fmt.Println("Creating a new project.")
+	}
+	fmt.Printf("Output will be written to: %s\n", *out)
+}
+
+func getPackageName(path string) (string, error) {
+	// Use build.Import to analyze the directory
+	pkg, err := build.ImportDir(path, 0)
+	if err != nil {
+		return "", err
+	}
+
+	return pkg.Name, nil
+}
+
+type methodDef struct {
+	TypeName, MethodName string
+}
+
+type configArg struct {
+	PkgName  string
+	BuildTag string
+	Methods  []methodDef
+}
+
+// getOutputWriter returns an io.WriteCloser for either a file or stdout based on the output path.
+func getOutputWriter(outputPath string) (io.WriteCloser, error) {
+	if outputPath == "" || outputPath == "--" {
+		// Use stdout
+		return os.Stdout, nil
+	}
+
+	// Open a file for writing
+	file, err := os.Create(outputPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create output file: %w", err)
+	}
+	return file, nil
 }
--- internal/builder/gen_schema.go
+++ internal/builder/gen_schema.go
@@ -1,19 +1,47 @@
 package builder
 
 import (
+	"errors"
+	"fmt"
+	"github.com/dave/dst"
 	"github.com/dave/dst/decorator"
 	"github.com/tylergannon/go-gen-jsonschema/internal/scanner"
+	"go/token"
+	"slices"
+	"strings"
 )
+
+const maxNestingDepth = 5
+
+type seenTypes []scanner.TypeID
+
+func (s seenTypes) Seen(t scanner.TypeID) bool {
+	return slices.Contains(s, t.Concrete())
+}
+
+func (s seenTypes) See(t scanner.TypeID) seenTypes {
+	return append(seenTypes{t.Concrete()}, s...)
+}
 
 func New(pkg *decorator.Package) (SchemaBuilder, error) {
 	var builder = SchemaBuilder{
 		LocalPkg: pkg,
+		Packages: map[string]scanner.ScanResult{},
+		Schemas:  map[scanner.TypeID]JSONSchema{},
 	}
 	data, err := scanner.LoadPackage(pkg)
 	if err != nil {
 		return builder, err
 	}
-	for _, _ = range data.SchemaMethods {
+	for _, m := range data.SchemaMethods {
+		if err = builder.mapType(m.Receiver, seenTypes{}); err != nil {
+			return builder, err
+		}
+	}
+	for _, f := range data.SchemaFuncs {
+		if err = builder.mapType(f.Receiver, seenTypes{}); err != nil {
+			return builder, err
+		}
 	}
 
 	return builder, nil
@@ -22,6 +50,7 @@ func New(pkg *decorator.Package) (SchemaBuilder, error) {
 type SchemaBuilder struct {
 	LocalPkg *decorator.Package
 	Packages map[string]scanner.ScanResult
+	Schemas  map[scanner.TypeID]JSONSchema
 }
 
 // loadScanResult gets the scan result associated with the given scanner.TypeID
@@ -40,17 +69,113 @@ func (s SchemaBuilder) loadScanResult(t scanner.TypeID) (scanner.ScanResult, err
 	return s.Packages[pkgPath], nil
 }
 
+func (s SchemaBuilder) find(t scanner.TypeID) (token.Position, error) {
+	sb, err := s.loadScanResult(t)
+	if err != nil {
+		return token.Position{}, err
+	}
+	typeSpec, ok := sb.LocalNamedTypes[t.TypeName]
+	if !ok {
+		return token.Position{}, fmt.Errorf("type %s not found", t.TypeName)
+	}
+	return scanner.NodePosition(sb.Pkg, typeSpec.TypeSpec), nil
+}
+
+func (s SchemaBuilder) mapInterface(iface scanner.IfaceImplementations, seen seenTypes) error {
+	if seen.Seen(iface.TypeID) {
+		return fmt.Errorf("circular dependency found for type %s, defined at %s", iface.TypeID, iface.Position)
+	}
+	seen = seen.See(iface.TypeID)
+	if err := s.checkSeen(seen); err != nil {
+		return err
+	}
+
+	node := UnionTypeNode{
+		TypeID_: iface.TypeID,
+	}
+	for _, opt := range iface.Impls {
+		if err := s.mapType(opt, seen); err != nil {
+			return err
+		}
+		optSchema := s.Schemas[opt]
+		obj, ok := optSchema.(ObjectNode)
+		if !ok {
+			pos, err := s.find(obj.TypeID_)
+			if err != nil {
+				return err
+			} else {
+				return fmt.Errorf("expected %s to be an object-type schema at %s", obj.TypeID_.TypeName, pos)
+			}
+		}
+		node.Options = append(node.Options, obj)
+	}
+	s.Schemas[iface.TypeID] = node
+	return nil
+}
+
+func (s SchemaBuilder) mapEnumType(enum *scanner.EnumSet, seen seenTypes) error {
+	seen = seen.See(enum.TypeID)
+	if err := s.checkSeen(seen); err != nil {
+		return err
+	}
+
+	propType := PropertyNode[string]{
+		TypeID_: enum.TypeID,
+		Typ:     "string",
+	}
+	for _, opt := range enum.Values {
+		propType.Enum = append(propType.Enum, strings.Trim(opt.Decl.Values[0].(*dst.BasicLit).Value, "\""))
+	}
+	s.Schemas[enum.TypeID] = propType
+	return nil
+}
+
 // mapType
-func (s SchemaBuilder) mapType(t scanner.TypeID, seen map[scanner.TypeID]bool) error {
+func (s SchemaBuilder) mapType(t scanner.TypeID, seen seenTypes) error {
 	scanResult, err := s.loadScanResult(t)
 	if err != nil {
 		return err
 	}
-	_ = scanResult
-
-	if _, ok := seen[t]; ok {
-
+	if iface, ok := scanResult.Interfaces[t.TypeName]; ok {
+		if err = s.mapInterface(iface, seen); err != nil {
+			return err
+		}
+	} else if enum, ok := scanResult.Constants[t.TypeName]; ok {
+		if err = s.mapEnumType(enum, seen); err != nil {
+			return err
+		}
+	} else if err = s.mapNamedType(t, seen); err != nil {
+		return err
 	}
 
 	return nil
+}
+
+func (s SchemaBuilder) checkSeen(seen seenTypes) error {
+	if len(seen) > maxNestingDepth {
+		pos, _ := s.find(seen[0])
+		return fmt.Errorf("max nesting depth exceeded at %s", pos)
+	}
+	return nil
+}
+
+func (s SchemaBuilder) mapNamedType(t scanner.TypeID, seen seenTypes) error {
+	scanResult, err := s.loadScanResult(t)
+	if err != nil {
+		return err
+	}
+	typeSpec, ok := scanResult.LocalNamedTypes[t.TypeName]
+	if !ok {
+		return fmt.Errorf("type %s not found", t.TypeName)
+	}
+	if seen.Seen(t) {
+		return fmt.Errorf("circular dependency found for type %s at %s", t.TypeName, typeSpec.Position())
+	}
+	switch node := typeSpec.TypeSpec.Type.(type) {
+	default:
+
+		fmt.Printf("Unrecognized node %T %#v type %s at %s\n", node, node, t.TypeName, typeSpec.Position())
+		return errors.New("unhandled node type")
+	}
+	//return nil
 }
