7ad52e421c65faf07fd6537f53c974bed1ecc25c
Tyler Gannon <tyler@pagerguild.com>
2025-01-26 14:59:43 -0600

    Structured Responses for test creation

---
+++ internal/builder/messages/schema.go
@@ -0,0 +1,34 @@
+//go:build jsonschema
+// +build jsonschema
+
+package messages
+
+import (
+	"encoding/json"
+
+	jsonschema "github.com/tylergannon/go-gen-jsonschema"
+)
+
+func (Assertion) Schema() (json.RawMessage, error) {
+	panic("not implemented")
+}
+
+func (ToolFuncGetTypeInfo) Schema() (json.RawMessage, error) {
+	panic("not implemented")
+}
+
+func (GeneratedTestResponse) Schema() (json.RawMessage, error) {
+	panic("not implemented")
+}
+
+var (
+	_ = jsonschema.NewJSONSchemaMethod(Assertion.Schema)
+	_ = jsonschema.NewJSONSchemaMethod(ToolFuncGetTypeInfo.Schema)
+	_ = jsonschema.NewJSONSchemaMethod(GeneratedTestResponse.Schema)
+	_ = jsonschema.NewInterfaceImpl[AssertionValue](
+		AssertNumericValue{},
+		AssertStringValue{},
+		AssertBoolValue{},
+		AssertType{},
+	)
+)
